a4e6ac21ef775fa0ae02f6573ed7c0fc6e4b9557
iamamansinghrajput <singhaman2321@gmail.com>
2025-06-06 22:37:39 +0530

    new project

---
+++ server/index.js
@@ -0,0 +1,13 @@
+// index.js
+const express = require('express');
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+
+
+require("./config/conn");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
