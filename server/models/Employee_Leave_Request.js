5e99dbef7d37941da2c891308b69c6665955852b
sushantpri <prince.sushant2004@gmail.com>
2025-06-07 15:25:36 +0530

    working on modeles sushant

---
+++ server/models/Employee_Leave_Request.js
@@ -0,0 +1,43 @@
+const mangooes = require('mangooes');
+const EmployeeLeaveRequestSchema = new mongooes.Schema(
+    {       // _id is acts as primary key that will be automatcally created by mangooDB
+        UserId : {
+            type: mangooes.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        Leave_Starting_Data : {
+            type: Date,
+            required: true,
+        },
+        Leave_Ending_Date : {
+            type: Date,
+            required: true,
+        },
+        Leave_Reason : {
+            type: String,
+            required: true,
+        },
+        Leave_Approval_Status : {
+            type: String,
+            enum: ['Pending', 'Approved', 'Rejected'],
+            default: 'Pending',
+        },
+        Admin_Send_Note : {
+            type: String,
+        },
+        Leave_Approved_By_AdminId : {   
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'User',        // Admin who approved the Leave of User
+        },
+        Leave_Request_Date : {
+            type: Date,
+            default: Date.now    // Submission_Day date (Automatically Filled)
+        },
+        MessageByUser : {
+            type: String,           // User send a note to Admin to Grant his/her Leave
+        }
+    }
+)
+let Employee_Leave_Request = mongoose.model('Employee_Leave_Request', EmployeeLeaveRequestSchema);
+module.exports = Employee_Leave_Request;
