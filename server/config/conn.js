1e7a16ffe7211f270e1c82b75ebcbc03ec782056
sushantpri <prince.sushant2004@gmail.com>
2025-06-08 22:18:43 +0530

    done

--- server/config/conn.js
+++ server/config/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const DB = "mongodb+srv://singhaman2321:N1EZgEd1b6GZvHMj@cluster0.4tsj5xz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
- 
+
 mongoose.connect(DB, {})
 .then(() =>{
     console.log("connection successfull")
--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 // index.js
-const express = require('express');
+const express = require("express");
+const leaveRequestRoutes = require("./routes/leaveRequestRoutes");
 
 const app = express();
 const port = 4000;
 
 app.use(express.json());
 
-
 require("./config/conn");
+
+app.use('/api', leaveRequestRoutes);
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
