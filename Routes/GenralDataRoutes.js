062e14adae3eec95952d0b23e645fbb4558681e5
Trikzmr <mrtrikz729@gmail.com>
2025-06-22 10:10:26 +0530

    fixing vercel

--- Routes/GenralDataRoutes.js
+++
@@ -1,9 +0,0 @@
-const express =require("express");
-const api =express.Router();
-const {allcalenders,monthCalender} = require("../controllers/GenralDataController");
-const employeeMiddleware = require("../middleware/employeeAuthentication");
-
-api.get("/getAllCalender",employeeMiddleware,allcalenders);
-api.post("/getCalender/:month",employeeMiddleware,monthCalender);
-
-module.exports=api;
--- Routes/monthlyReportRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const api = express.Router();
-const  monthlyReportGeneration = require("../utils/monthlyReportGeneration");
-
-api.post('/monthlyReportGenerationOfEachUsers', monthlyReportGeneration);
-
-module.exports = api;
