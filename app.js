cb20c16276f4deeb18a078071eeb3769c02599bc
Trikzmr <mrtrikz729@gmail.com>
2025-06-22 10:36:30 +0530

    fixing vercel

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const resignationRoutes = require("./Routes/resignationRoutes");
 const base = require("./Routes/Base")
 const userMonthReport = require("./Routes/userMonthReport")
 const adminRoutes = require("./Routes/AdminRoutes");
-const genralDataRoutes =require("./Routes/GenralDataRoutes");
+//const genralDataRoutes =require("./Routes/GenralDataRoutes");
 const app = express();
  
 const port = 3005;
