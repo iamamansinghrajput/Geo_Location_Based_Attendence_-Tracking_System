becb8499820db4c9af03c22ed687a1959fb8c176
Trikzmr <mrtrikz729@gmail.com>
2025-06-22 10:20:09 +0530

    fixing vercel

---
+++ Routes/GenralDataRoutes.js
@@ -0,0 +1,9 @@
+const express =require("express");
+const api =express.Router();
+const {allcalenders,monthCalender} = require("../controllers/GenralDataController");
+const employeeMiddleware = require("../middleware/employeeAuthentication");
+
+api.get("/getAllCalender",employeeMiddleware,allcalenders);
+api.post("/getCalender/:month",employeeMiddleware,monthCalender);
+
+module.exports=api;
---
+++ Routes/leaveRequestRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const api = express.Router();
+const { employeeAddLeaveRequest, seeAllLeaveRequest, findLeaveRequestByProvidedId, approveLeaveRequest, rejectLeaveRequest, deleteLeaveRequest} = require("../controllers/leaveRequestControllers");
+
+api.post('/addLeaveRequest', employeeAddLeaveRequest);
+api.get('/getAllLeaveRequest', seeAllLeaveRequest);
+api.post('/getLeaveRequestByid', findLeaveRequestByProvidedId);
+api.put('/approveLeaveRequest/:id', approveLeaveRequest);
+api.put('/RejectLeaveRequest/:id', rejectLeaveRequest);
+api.delete('/DeleteLeaveRequest/:id', deleteLeaveRequest);
+
+module.exports = api;
