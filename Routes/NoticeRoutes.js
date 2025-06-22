60a7dbf13841d2098a90092c804e14347413dcb5
Trikzmr <mrtrikz729@gmail.com>
2025-06-22 10:18:30 +0530

    fixing vercel

--- Routes/NoticeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express =require("express");
-const api =express.Router();
-const {postNotice,getAllNotices} = require("../controllers/NoticeControllers");
-
-api.post("/addNotice",postNotice);
-api.get("/getNotice",getAllNotices);
-
-module.exports=api;
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
-const base = require('./routes/Base'); 
+const base = require('./Routes/Base'); 
 const app = express(); 
 const port = 3005;
  
