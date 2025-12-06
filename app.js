223fff4cf67e8611359112effaf325077b38a85f
iamamansinghrajput <singhaman2321@gmail.com>
2025-12-06 12:16:41 +0530

    cors fix

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 const port = 3005;
  
 app.use(cors({
-  origin: ["http://localhost:5173","http://localhost:8081"], 
+  origin: ["http://localhost:5173","http://localhost:8081", "http://localhost:5174"], 
   credentials: true,
 }));
 app.use(express.json());
